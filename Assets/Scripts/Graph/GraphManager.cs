eb901afd27e85cad1259ba7ef44c96ba2bf23078
Thomas Lamb <thomaslambprogramming@gmail.com>
2024-05-15 11:44:03 +1000

    Added more options to graph manager

--- Assets/Scripts/Graph/GraphManager.cs
+++ Assets/Scripts/Graph/GraphManager.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Graph
 {
     public class GraphManager : MonoBehaviour
     {
         [SerializeField] private GameObject m_GraphPrefab;
-        private GameObject[] m_GraphPoints;
-        [SerializeField] private float step = 2f;
+        private Transform[] m_GraphPoints;
+        [SerializeField] private float m_GraphStep = 2f;
         [SerializeField] private int m_GraphCount;
         [SerializeField] private float m_GraphPointScale = 0.1f;
         [SerializeField] private float m_TimeMultiplier = 1.5f;
         [SerializeField] private bool m_RemakeGraph = false;
+        [SerializeField] private bool m_UpdateGraphOnFrame = true;
 
         [SerializeField, Range(0.01f, 20)] private float m_TimeOffsetForMultiwave;
         [SerializeField] private LibraryFunctions.WaveFunctions m_Function = LibraryFunctions.WaveFunctions.Wave;
@@ -22,37 +24,13 @@ namespace Graph
 
         private void Update()
         {
-            if (m_GraphPoints == null)
-            {
-                m_GraphPoints = new GameObject[transform.childCount];
-                for (int i = 0; i < transform.childCount; i++)
-                {
-                    m_GraphPoints[i] = transform.GetChild(i).gameObject;
-                }
-                m_RemakeGraph = true;
-            }
-            if (m_RemakeGraph)
+            if (m_RemakeGraph || m_GraphPoints == null || (m_GraphPoints.Length != m_GraphCount * m_GraphCount))
             {
                 RemakeGraphArray();
                 m_RemakeGraph = false;
             }
-            for (int i = 0; i < m_GraphPoints.Length; i++)
-            {
-                Vector3 localPos = m_GraphPoints[i].transform.localPosition;
-                switch (m_Function)
-                {
-                    case LibraryFunctions.WaveFunctions.Wave:
-                        localPos.y = LibraryFunctions.Wave(localPos.x, Time.time * m_TimeMultiplier);
-                        break;
-                    case LibraryFunctions.WaveFunctions.MultiWave:
-                        localPos.y = LibraryFunctions.MultiWave(localPos.x, m_TimeOffsetForMultiwave, Time.time * m_TimeMultiplier);
-                        break;
-                    case LibraryFunctions.WaveFunctions.Ripple:
-                        localPos.y = LibraryFunctions.Ripple(localPos.x, Time.time * m_TimeMultiplier);
-                        break;
-                }
-                m_GraphPoints[i].transform.localPosition = localPos;
-            }
+            if (m_UpdateGraphOnFrame)
+                UpdateGraphArray();
         }
 
         [ContextMenu("Remake Graph Array")]
@@ -63,21 +41,55 @@ namespace Graph
         }
         private void CreateGraphArray()
         {
-            m_GraphPoints = new GameObject[m_GraphCount];
+            //Graph count = amount in x and z
+            m_GraphPoints = new Transform[m_GraphCount * m_GraphCount];
             for (int i = 0; i < m_GraphPoints.Length; i++)
             {
-                m_GraphPoints[i] = Instantiate(m_GraphPrefab, this.transform);
-                float xOffset = -m_GraphCount * step / 2 + 1;
-                m_GraphPoints[i].transform.position = new Vector3(xOffset + (i + 0.5f) * step - 1f, 0, 0);
+                m_GraphPoints[i] = Instantiate(m_GraphPrefab, transform).transform;
+            }
+            UpdateGraphArray();
+        }
+
+        private void UpdateGraphArray()
+        {
+            float time = Time.time;
+            LibraryFunctions.GraphFunction graphFunction = LibraryFunctions.GetFunction(m_Function);
+            //What the fuck. didn't know this was a thing. cool.
+            for (int i = 0, x = 0, z = 0; i < m_GraphPoints.Length; i++, x++)
+            {
+                if (x == m_GraphCount)
+                {
+                    x = 0;
+                    z++;
+                }
+
+                //Attempt at making the grid be centered around the 0,0,0 instead of going off 0 then continuing positively
+                float halfOffset = -m_GraphCount * m_GraphStep / 2 + 1;
+                
+                float u = halfOffset + (x + 0.5f) * m_GraphStep - 1f;
+                float v = halfOffset + (z + 0.5f) * m_GraphStep - 1f; 
+                
+                m_GraphPoints[i].transform.localPosition = graphFunction(u,v,time * m_TimeMultiplier);;
                 m_GraphPoints[i].transform.localScale = new Vector3(m_GraphPointScale, m_GraphPointScale, m_GraphPointScale);
             }
         }
+
         private void DestroyGraphArray()
         {
-            for (int i = 0; i < m_GraphPoints.Length; i++)
+            if (transform.childCount != 0)
             {
-                Destroy(m_GraphPoints[i].gameObject);
+                m_GraphPoints = new Transform[transform.childCount];
+                for (int i = 0; i < transform.childCount; i++)
+                    m_GraphPoints[i] = transform.GetChild(i);
             }
+
+            if (m_GraphPoints != null)
+            {
+                for (int i = 0; i < m_GraphPoints.Length; i++)
+                    Destroy(m_GraphPoints[i].gameObject);
+            }
+
+            m_GraphPoints = null;
         }
     }
 }
--- Assets/Scripts/Graph/LibraryFunctions.cs
+++ Assets/Scripts/Graph/LibraryFunctions.cs
@@ -4,32 +4,66 @@ namespace Graph
 {
     public static class LibraryFunctions
     {
-        public enum WaveFunctions {Wave, MultiWave, Ripple}
+        public enum WaveFunctions {Wave, MultiWave, Ripple, Sphere}
 
-        public delegate float Function(float a_X, float a_Time);
-        public static float Wave(float a_XPos, float a_Time)
+        public static GraphFunction GetFunction(WaveFunctions function) => functions[(int)function];
+        public delegate Vector3 GraphFunction(float u, float v, float time);
+
+        private static GraphFunction[] functions = { Wave, MultiWave, Ripple, Sphere};
+        
+        private static Vector3 Wave(float u, float v, float time)
         {
+            Vector3 p;
+            p.x = u;
+            p.z = v;
+            
             //Simple Sine wave
-            return Sin(PI * (a_XPos + a_Time));
+            p.y = Sin(PI * (u + v + time));
+            return p;
         }
-        public static float MultiWave(float a_X, float a_TimeOffset, float a_Time)
+        private static Vector3 MultiWave(float u, float v, float time)
         {
+            Vector3 p;
+            p.x = u;
+            p.z = v;
             //Get a simple sin wave and add another sine wave ontop of it.
             //this creates an effect where the the 2nd sine wave is sliding ontop of the previous.
             //because the 2nd sine wave is occuring at twice the rate and we have two sine waves, we limit
             //to -1, 1 by dividing the result by 1.5 as its max amplitude is 1.5.
-            float y = Sin(PI * (a_X + a_TimeOffset * a_Time));
-            y += Sin(2f * PI * (a_X + a_Time)) * 0.5f;
-            return y / 1.5f;
+            p.y = Sin(PI * (u + time));
+            //To add something interesting we add the z position just to the second sine wave.
+            p.y += Sin(2f * PI * (v + time)) * 0.5f;
+            //add a third wave with x and z but have its timing a quarter frequency
+            p.y += Sin(PI * (v + u + time * 0.25f));
+            p.y *= (1f / 2.5f);
+            return p;
         }
-        public static float Ripple(float a_X, float a_Time)
+        private static Vector3 Ripple(float u, float v, float time)
         {
+            Vector3 p;
+            p.x = u;
+            p.z = v;
             //Abs so its always the same distance on either side, since we have mirrored values we divide its final
             //result so that the amplitude is not massive and unreadable, by having 10*d division it also ensures the value
             //is reduced the further away the point is.
-            float d = Abs(a_X);
-            float y = Sin(4f * PI * (d - a_Time));
-            return y / (1f + 10f * d);
+            //float d = Abs(a_X);
+
+            //Get distance from 0,0,0
+            float d = Sqrt(u * u + v * v);
+            p.y = Sin(4f * PI * (d - time));
+            p.y /= (1f + 10f * d);
+            return p;
+        }
+
+        private static Vector3 Sphere(float u, float v, float time)
+        {
+            Vector3 p;
+            float r = Cos(0.5f * PI * v);
+            p.x = r * Sin(PI * u);
+            p.z = r * Cos(PI * u);
+            p.y = Sin(PI * 0.5f * v);
+
+            return p;
         }
     }
 }
